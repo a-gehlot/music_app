ae67f5277c4d51251bbdfe7816376e8684067fd9
AGehlot <andrew.gehlot@gmail.com>
2020-06-26 18:17:53 -0400

    Added method to find/verify users logging in

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,12 @@ class User < ApplicationRecord
 
     after_initialize :ensure_session_token
 
+    def self.find_by_credentials(email, password)
+        user = User.find_by(email: email)
+        return nil if user.nil?
+        user.is_password?(password) ? user : nil
+    end
+
     def self.generate_session_token
         SecureRandom.urlsafe_base64(16)
     end
